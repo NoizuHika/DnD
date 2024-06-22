8f99155bdf9f8ff563e7244aacb86be7b5460fa4
Aleksander Mielczarek <s15465@pjwstk.edu.pl>
2024-06-22 17:14:33 +0200

    Update Character1.tsx

--- Character1.tsx
+++ Character1.tsx
@@ -1,110 +1,117 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ImageBackground, StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import PlayerCharacter from './PlayerCharacter'; // Import if using separate file
 
 const Character1 = ({ navigation }) => {
-  const handleGoBack = () => {
-    navigation.navigate('Characters');
-  };
-
   const { t, i18n } = useTranslation();
-  const calculateLargerNumber = (value) => {
-      const largerNumber = Math.floor((value - 10) / 2);
-      return largerNumber >= 0 ? `+${largerNumber}` : `${largerNumber}`;
-    };
-  // Example of creating a new PlayerCharacter instance
-  const player = new PlayerCharacter(8, 15, 12, 13, 14, 15, 16, 17);
-  /*
   const [characterData, setCharacterData] = useState(null);
+
   useEffect(() => {
-        fetchData();
-      }, []);
+    fetchData();
+  }, []);
 
   const fetchData = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:8000/player_characters/0');
+      const response = await fetch('http://10.0.2.2:8000/player_characters/0');
       if (!response.ok) {
         throw new Error('Failed to fetch data');
       }
       const data = await response.json();
-      // Assuming data is in the format { STR, DEX, CON, INT, WIS, CHA, AC, INIT }
       setCharacterData(data);
     } catch (error) {
       console.error('Error fetching data:', error);
       // Handle error fetching data
     }
-    const player = new PlayerCharacter(
-        characterData.strScore,
-        characterData.dexScore,
-        characterData.conScore,
-        characterData.intScore,
-        characterData.wisScore,
-        characterData.chaScore,
-        characterData.armorClass,
-        calculateLargerNumber(characterData.dexScore) // Assuming INIT value is not provided in the schema, using 0 for now
-     );
   };
-  */
-  // Function to calculate the larger number
+
+  const handleGoBack = () => {
+    navigation.navigate('Characters');
+  };
 
   const handleStatPress = () => {
-       navigation.navigate('RzutKostka_Bonus');
-    };
+    navigation.navigate('RzutKostka_Bonus');
+  };
+
+  const calculateLargerNumber = (value) => {
+    const largerNumber = Math.floor((value - 10) / 2);
+    return largerNumber >= 0 ? `+${largerNumber}` : `${largerNumber}`;
+  };
+
+  // Render conditionally based on whether characterData is fetched
+  if (!characterData) {
+    return (
+      <View style={styles.loadingContainer}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  // Create PlayerCharacter instance after characterData is available
+  const player = new PlayerCharacter(
+    characterData.strScore,
+    characterData.dexScore,
+    characterData.conScore,
+    characterData.intScore,
+    characterData.wisScore,
+    characterData.chaScore,
+    characterData.armorClass,
+    calculateLargerNumber(characterData.dexScore)
+  );
 
   return (
     <ImageBackground
       source={require('./assets/dungeon.jpeg')}
       style={styles.container}
     >
-    <View style={styles.imageContainer}>
-             <Image source={require('./assets/assasin.jpeg')} style={styles.image} />
-       </View>
+      <View style={styles.imageContainer}>
+        <Image source={require('./assets/assasin.jpeg')} style={styles.image} />
+      </View>
       <View style={styles.GoBack}>
         <TouchableOpacity style={styles.button} onPress={handleGoBack}>
           <Text style={styles.GoBackText}>{t('Go_back')}</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.statsContainer}>
-            <TouchableOpacity onPress={() => handleStatPress()}>
 
-        <View style={styles.statBox}>
-          <Text style={styles.largeText}>{calculateLargerNumber(player.STR)}</Text>
-          <Text style={styles.statText}>{`STR: ${player.STR}`}</Text>
+        <View style={styles.statsContainer}>
+        <TouchableOpacity onPress={handleStatPress}>
+          <View style={styles.statBox}>
+            <Text style={styles.largeText}>{calculateLargerNumber(player.STR)}</Text>
+            <Text style={styles.statText}>{`STR: ${player.STR}`}</Text>
+          </View>
+          <View style={styles.statBox}>
+            <Text style={styles.largeText}>{calculateLargerNumber(player.DEX)}</Text>
+            <Text style={styles.statText}>{`DEX: ${player.DEX}`}</Text>
+          </View>
+          <View style={styles.statBox}>
+            <Text style={styles.largeText}>{calculateLargerNumber(player.CON)}</Text>
+            <Text style={styles.statText}>{`CON: ${player.CON}`}</Text>
+          </View>
+          <View style={styles.statBox}>
+            <Text style={styles.largeText}>{calculateLargerNumber(player.INT)}</Text>
+            <Text style={styles.statText}>{`INT: ${player.INT}`}</Text>
+          </View>
+          <View style={styles.statBox}>
+            <Text style={styles.largeText}>{calculateLargerNumber(player.WIS)}</Text>
+            <Text style={styles.statText}>{`WIS: ${player.WIS}`}</Text>
+          </View>
+          <View style={styles.statBox}>
+            <Text style={styles.largeText}>{calculateLargerNumber(player.CHA)}</Text>
+            <Text style={styles.statText}>{`CHA: ${player.CHA}`}</Text>
+          </View>
+          </TouchableOpacity>
         </View>
-        <View style={styles.statBox}>
-          <Text style={styles.largeText}>{calculateLargerNumber(player.DEX)}</Text>
-          <Text style={styles.statText}>{`DEX: ${player.DEX}`}</Text>
-        </View>
-        <View style={styles.statBox}>
-          <Text style={styles.largeText}>{calculateLargerNumber(player.CON)}</Text>
-          <Text style={styles.statText}>{`CON: ${player.CON}`}</Text>
-        </View>
-        <View style={styles.statBox}>
-          <Text style={styles.largeText}>{calculateLargerNumber(player.INT)}</Text>
-          <Text style={styles.statText}>{`INT: ${player.INT}`}</Text>
-        </View>
-        <View style={styles.statBox}>
-          <Text style={styles.largeText}>{calculateLargerNumber(player.WIS)}</Text>
-          <Text style={styles.statText}>{`WIS: ${player.WIS}`}</Text>
-        </View>
-        <View style={styles.statBox}>
-          <Text style={styles.largeText}>{calculateLargerNumber(player.CHA)}</Text>
-          <Text style={styles.statText}>{`CHA: ${player.CHA}`}</Text>
-        </View>
-        </TouchableOpacity>
-       </View>
 
-        <View style={styles.leftContainer}>
-            <View style={styles.circleBox}>
-                <Text style={styles.circleText}>{player.AC}</Text>
-                <Text style={styles.circleLabel}>AC</Text>
-            </View>
-            <View style={styles.circleBox}>
-                <Text style={styles.circleText}>{player.INIT}</Text>
-                <Text style={styles.circleLabel}>Initiative</Text>
-            </View>
+      <View style={styles.leftContainer}>
+        <View style={styles.circleBox}>
+          <Text style={styles.circleText}>{player.AC}</Text>
+          <Text style={styles.circleLabel}>AC</Text>
         </View>
+        <View style={styles.circleBox}>
+                  <Text style={styles.circleText}>{player.INIT}</Text>
+                  <Text style={styles.circleLabel}>Initiative</Text>
+        </View>
+      </View>
     </ImageBackground>
   );
 };
@@ -159,31 +166,31 @@ const styles = StyleSheet.create({
     color: '#d6d6d6',
   },
   leftContainer: {
-      position: 'absolute',
-      top: 70,
-      left: 20,
-    },
+    position: 'absolute',
+    top: 70,
+    left: 20,
+  },
   circleBox: {
-      marginVertical: 10,
-      width: 70,
-      height: 70,
-      borderRadius: 100,
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      borderColor: '#7F7F7F',
-      borderWidth: 1.5,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    circleText: {
-      fontSize: 24,
-      color: '#ffffff',
-    },
-    circleLabel: {
-      fontSize: 12,
-      color: '#d6d6d6',
-      marginTop: 2,
-    },
-imageContainer: {
+    marginVertical: 10,
+    width: 70,
+    height: 70,
+    borderRadius: 100,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    borderColor: '#7F7F7F',
+    borderWidth: 1.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  circleText: {
+    fontSize: 24,
+    color: '#ffffff',
+  },
+  circleLabel: {
+    fontSize: 12,
+    color: '#d6d6d6',
+    marginTop: 2,
+  },
+  imageContainer: {
     position: 'absolute',
     top: '10%',
     justifyContent: 'center',
@@ -199,6 +206,11 @@ imageContainer: {
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
